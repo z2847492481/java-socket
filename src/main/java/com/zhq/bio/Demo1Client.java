db78b36d0fcae88794fe723b6b4f15dadd307c7c
zhq123 <2847492481@qq.com>
2024-02-03 21:22:55 +0800

    add log4j2

---
+++ src/main/java/com/zhq/bio/Demo1Client.java
@@ -0,0 +1,14 @@
+package com.zhq.bio;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @DATE: 2024-02-03 18:35
+ * @Author: zhq123
+ */
+@Slf4j
+public class Demo1Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/zhq/bio/Demo1Server.java
@@ -0,0 +1,8 @@
+package com.zhq.bio;
+
+/**
+ * @DATE: 2024-02-03 18:35
+ * @Author: zhq123
+ */
+public class Demo1Server {
+}
